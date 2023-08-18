a02c0aa292b0b1314f2b9b5eac6919b60235111b
Dennis Josefsson <dennis.josefsson@gmail.com>
2023-08-18 17:13:47 +0200

    Finalized responsive web, version 1.0

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,8 @@ app.use((req, res, next) => {
   next()
 })
 app.use(cookieParser())
-// const frontend = path.join(__dirname, 'dist')
-// app.use('/', express.static(frontend))
+const frontend = path.join(__dirname, 'dist')
+app.use('/', express.static(frontend))
 
 app.use('/api/healthCheck', mainRouter)
 app.use(APIRestrictMiddleware)
@@ -40,9 +40,9 @@ app.use('/api/metadata', metadataRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/links', linkRouter)
 
-// app.use((req, res, next) => {
-//   res.sendFile(path.join(frontend, 'index.html'))
-// })
+app.use((req, res, next) => {
+  res.sendFile(path.join(frontend, 'index.html'))
+})
 
 app.use(errorHandler)
 
