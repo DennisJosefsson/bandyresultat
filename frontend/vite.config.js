d3bf0bce10df5861e7dec943fff0de16fce45ae9
Dennis Josefsson <dennis.josefsson@gmail.com>
2024-02-16 14:29:17 +0100

    Changed chunk build sizes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin(), visualizer()],
   server: {
     headers: { 'Cross-Origin-Opener-Policy': 'same-origin-allow-popups' },
   },
   assetsInclude: ['**/*.PNG'],
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (
+  //           id.includes('leaflet') ||
+  //           id.includes('react-leaflet') ||
+  //           id.includes('react-leaflet-cluster') ||
+  //           id.includes('leaflet.markercluster')
+  //         )
+  //           return 'leaflet'
+  //       },
+  //     },
+  //   },
+  // },
 })
